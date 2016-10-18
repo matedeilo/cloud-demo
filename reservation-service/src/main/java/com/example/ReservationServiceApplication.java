55c3bf3b7d1cf297a5aca2fdab7118b5dad4d57c
Manuel Alejandro Tejeda Pinto <mtejeda@avantica.net>
2016-10-18 17:49:21 -0500

    fx2

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -1,12 +1,17 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,6 +23,23 @@ public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+}
+
+@RestController
+@RefreshScope
+class MessageRestController {
+
+	private final String value;
+
+	@Autowired
+	public MessageRestController(@Value("${message}") String value) {
+		this.value = value;
+	}
+
+	@RequestMapping(method =RequestMethod.GET, value= "/message")
+	String read(){
+		return this.value;
 	}
 }
 
